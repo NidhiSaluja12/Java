dcb91e85fe1f9580f82f8a8c992cb456d20831f2
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-14 19:27:37 +0530

    Create Ternary Operator(Even or odd).java

---
+++ Ternary Operator(Even or odd).java
@@ -0,0 +1,14 @@
+package class_I;
+
+public class ternary_operator {
+	
+	public static void main(String[] args) {
+		
+		int n = 34;
+		String ans = "";
+		ans = (n%2==0)?"even":"odd";
+		System.out.println(n+" is "+ans);
+		
+	}
+
+}
