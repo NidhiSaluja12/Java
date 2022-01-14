5acfcd0b4a101f3c7344ca3274390ba922709d9f
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-14 19:28:19 +0530

    Create Ternary operator (greatest among three).java

---
+++ Ternary operator (greatest among three).java
@@ -0,0 +1,17 @@
+package class_I;
+
+public class greatest {
+	public static void main(String args[]) {
+		
+		int a=12340,b=200,c=30,greatest;
+	
+		greatest = (a>b)&&(a>c)?a:(b>c)?b:c;
+		System.out.println(greatest);
+		
+	}
+		
+}
+	
+
+
+
