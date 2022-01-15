c30c44155d41730c138a72413d869f04a0a99130
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-15 23:09:14 +0530

    Create Switch case 2.java

---
+++ Switch case 2.java
@@ -0,0 +1,31 @@
+package class_2;
+import java.util.Scanner;
+
+public class switch_case2 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		String status="";
+		System.out.println("Enter the number: ");
+		int num = sc.nextInt();
+		
+		switch(num){
+		
+		case 25:  status = "Right";
+				break;
+		case 50:  status = "Right";
+				break;
+		case 75:  status = "Right";
+				break;
+		case 100:  status = "Right";
+				break;
+		default : System.out.println("Invalid choice..");
+		
+		
+		}
+		
+		System.out.println(status);
+			
+	}
+	
+}
