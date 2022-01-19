1728e9a7298dd3195675040f9eec08007a8566bd
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-19 23:42:33 +0530

    Create Loop 1 doWhile and while.java

---
+++ Loop 1 doWhile and while.java
@@ -0,0 +1,18 @@
+public class loop2_doWhile {
+	public static void main(String args[]) {
+		
+	int n = 1;
+//	do {
+//		System.out.println("Welcome"+n);
+//		//n++ ;
+//		n += 2;
+//	}while (n <= 10);
+//		
+	
+	while(n <= 10) {
+		System.out.println("Welcome"+n);
+		//n++ ;
+		n += 2;
+	}
+		
+	}
