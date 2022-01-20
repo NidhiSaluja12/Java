981eb0e82b6166a8ac917ac1ac9f20eb19fba42d
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-20 23:44:33 +0530

    Create Loop 4 while and doWhile.java

---
+++ Loop 4 while and doWhile.java
@@ -0,0 +1,27 @@
+package class_3;
+
+public class loop4_doWhile3 {
+public static void main(String args[]) {
+		
+		int n = 15;
+
+//		do {
+//			System.out.println("Square of "+n+"is"+n*n);
+//		    //n-- ;
+//			n += 1;
+//			
+//		}while(n<= 10);
+//			
+		
+		while(n<= 10) {
+			System.out.println("Square of "+n+"is"+n*n);
+		    //n-- ;
+			n += 1;
+			
+		}
+			
+		
+		System.out.println("Out of the loop because n is already greater than 10");
+	}
+
+}
