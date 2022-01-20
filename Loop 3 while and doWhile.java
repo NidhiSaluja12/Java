eac1f8eaf4e6658b8ab9e70ecad5abd9e266881c
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-20 23:43:59 +0530

    Create Loop 3 while and doWhile.java

---
+++ Loop 3 while and doWhile.java
@@ -0,0 +1,25 @@
+package class_3;
+
+public class loops3_doWhile2 {
+	
+	public static void main(String args[]) {
+		
+		int n = 10;
+//		do {
+//			System.out.println("Welcome"+n);
+//		    //n-- ;
+//			n -= 2;
+//			
+//		}while (n >= 1);
+	
+		while (n >= 1) {
+			System.out.println("Welcome"+n);
+		    //n-- ;
+			n -= 2;
+			
+		}
+			
+		
+	}
+
+}
