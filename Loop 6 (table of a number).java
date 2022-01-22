72b70212c1ebda6c001d1e0c9a3d4385c04b4e33
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-22 23:04:18 +0530

    Create Loop 6 (table of a number).java

---
+++ Loop 6 (table of a number).java
@@ -0,0 +1,32 @@
+package class_3;
+import java.util.Scanner;
+
+public class loop6_table {
+	public static void main(String args[]) {
+		//int n = 5, i =1;
+		
+		
+//		do {
+//			System.out.println(n * i);
+//			i++;
+//			
+//		}while (i <= 10);
+		
+		// all the applications in phone work on do while pattern - ek baar toh execution hoga hi
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter: ");
+		int n = sc.nextInt();
+		int i =1;
+		do {
+			System.out.println(n*i);
+			i ++;
+		}while(i<=10);
+		
+		
+	}
+
+}
+
+//max/min ; user se number 
