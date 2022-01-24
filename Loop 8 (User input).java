dbd98b3bd0c0abd4eef02ff012ab592b87aaaf9d
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-24 23:37:23 +0530

    Create Loop 8 (User input).java

---
+++ Loop 8 (User input).java
@@ -0,0 +1,27 @@
+package class_3;
+import java.util.Scanner;
+
+public class loop7_userInput {
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		char choice;
+		int n, count = 0 ;
+		
+		do {
+			System.out.println("Enter a number: ");
+			n = sc.nextInt();
+			count ++;
+			System.out.println("The number is: "+n);
+			System.out.println("Do you want to add more numbers?");
+			choice = sc.next().charAt(0);
+			
+			}while(choice=='Y'||choice=='y');
+	
+		System.out.println("Your have entered "+count+" values");	
+		
+	}
+	
+
+}
