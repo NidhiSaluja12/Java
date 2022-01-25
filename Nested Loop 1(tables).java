f971c867479e5b02e87e3455bd0ab22c8d11fa98
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-25 18:46:03 +0530

    Create Nested Loop 1(tables).java

---
+++ Nested Loop 1(tables).java
@@ -0,0 +1,22 @@
+package class_4;
+
+public class nestedLoop1 {
+	
+	public static void main(String args[]) {
+		
+	int n = 15;
+	do {
+		int i =1;
+		do {
+			System.out.print(n*i+" ");
+			i++;
+			
+			}while(i<=10);
+	System.out.println();
+		
+	n ++;
+	}while(n<=3);
+	System.out.println("Bye!");
+		
+	}
+}
