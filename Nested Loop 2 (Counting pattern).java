0fe251edf4d52041c5173a905cbd238017bb144b
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-26 23:49:14 +0530

    Create Nested Loop 2 (Counting pattern).java

---
+++ Nested Loop 2 (Counting pattern).java
@@ -0,0 +1,20 @@
+package class_4;
+
+public class nestedLoop2_doWhile {
+public static void main(String args[]) {
+	int m,n;
+	m = 1;
+	do {
+		n = 1;
+		do {
+			System.out.print(n+ "");
+			n ++;
+			
+		}while (n <= m);
+		System.out.println();
+		m++;
+		
+		
+	}while(m<=10);
+}
+}
