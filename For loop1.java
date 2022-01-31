4a509f6187aef4a854e198349db8353d7c27ad37
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-01-31 23:18:20 +0530

    Create For loop1.java

---
+++ For loop1.java
@@ -0,0 +1,32 @@
+package class_5;
+
+public class forLoop1 {
+	
+	public static void main(String args[]) {
+		for(int i=1;i<=10;i++) {
+			
+			System.out.print(i);
+		}
+		System.out.println();
+		
+		
+//		int i=1;
+//		
+//		for(;;) {
+//			
+//			System.out.print(i);
+//			i+=2;
+//			
+//			if (i>10) 
+//				break;
+//		}
+		
+		for(int i=1,j=20,k=100;i<=10;i++,j--,k+=10) { //condition(break) koi ek lagayega 
+			
+			System.out.println(i+" "+j+" "+k);
+		}
+
+
+	}
+
+}
