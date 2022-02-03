4dd7755341473a564fd05aacf8a35514d0fcd804
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-02-03 22:32:10 +0530

    Create Equilateral triangle pattern.java

---
+++ Equilateral triangle pattern.java
@@ -0,0 +1,55 @@
+package class_4;
+import java.util.Scanner;
+
+public class triangle_pattern1 {
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int c=1,r,n;
+		System.out.println("Number of lines: ");
+		n = sc.nextInt();
+		
+//		while(c<=n) {
+//			
+//			r=c;
+//			while(r<n) {
+//				System.out.print(" ");											
+//				r++;	
+//			}
+//			
+//			r=1;
+//			while(r<=c) {
+//				System.out.print("* ");
+//				
+//				r++;
+//			}
+//			
+//			System.out.println();
+//				c++;
+//		}
+//		
+		 do{
+			
+			r=c;
+			do {
+				System.out.print(" ");											
+				r++;	
+			}while(r<=n);
+			
+			r=1;
+			do {
+				System.out.print("* ");
+				
+				r++;
+			}while(r<=c);
+			
+			System.out.println();
+				c++;
+		}while(c<=n);
+
+		
+		
+	}
+
+}
