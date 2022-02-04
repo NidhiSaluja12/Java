32b9110c949ded1b29fbcefe23674a2910066775
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-02-04 18:05:13 +0530

    Create Fot loop 2.java

---
+++ Fot loop 2.java
@@ -0,0 +1,34 @@
+package class_5;
+import java.util.Scanner;
+
+public class forLoop2 {
+	public static void main(String args[]) {
+		
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number: ");
+	int n=sc.nextInt();
+	int f = 1;
+//	for (int i=1; i<=n; i++) {
+//		
+//		f = f*i;
+//	}
+	
+	
+	
+//	int i=1;
+//	while(i<=n) {
+//		f = f*i;
+//		i++;
+//		
+//	}
+	
+	int i=1;
+	do{
+		f = f*i;
+		i++;
+		
+	}while(i<=n);
+		System.out.println("Factorial: "+f);	
+	}
+
+}
