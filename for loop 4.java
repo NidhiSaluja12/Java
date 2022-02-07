0377d95f2a62ec4fe73536138bbf1f6f30353bb8
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-02-07 23:23:01 +0530

    Create for loop 4.java

---
+++ for loop 4.java
@@ -0,0 +1,28 @@
+package class_5;
+import java.util.Scanner;
+
+public class forloop4_trianglePattern {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		int c,r,n;
+		System.out.println("Number of lines: ");
+		n = sc.nextInt();
+		
+		for(c=1;c<=n;c++) {
+			
+			for(r=c;r<n;r++) {
+				System.out.print(" ");
+			}
+			
+			for(r=1;r<=c;r++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
+
+}
