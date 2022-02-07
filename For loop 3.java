71b0afb6cab1cd2101ea6a8429d432fec4067e16
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2022-02-07 23:22:27 +0530

    Create For loop 3.java

---
+++ For loop 3.java
@@ -0,0 +1,17 @@
+package class_5;
+
+public class forLoop3_nested {
+	public static void main(String args[]) {
+		int i,j;
+		for (i=2;i<=30;i++) {
+			for (j=1;j<=10;j++) {
+				
+				System.out.print(i*j+" ");
+				
+			}
+			System.out.println();
+			
+		}
+		
+	}
+}
